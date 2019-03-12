69e35b31c1cd8480eaec89cc274412bd7de8e4dc
Austin Jackson <themacphage@gmail.com>
2019-03-12 21:57:30 +0000

    can run with python -m expense-report

---
+++ expense-report/__init__.py
@@ -0,0 +1,10 @@
+from .version import __version__
+#from .shout import shout_and_repeat
+#from .add import my_add
+#from .app import app
+
+# if somebody does "from somepackage import *", this is what they will
+# be able to access:
+__all__ = [
+    'app'
+]
---
+++ expense-report/app.py
@@ -0,0 +1,5 @@
+def run():
+	print("hello")
+
+if __name__ == '__main__':
+	run() 
